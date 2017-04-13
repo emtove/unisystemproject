c98ef1d0dd537468aac61a9be7d3df393d82dda7
emtove <emelie.ohrvall@gmail.com>
2017-04-13 11:18:08 +0200

    Cleaned up the redirection code for when you need to log in

--- src/main/java/login/filters/AuthFilter.java
+++ src/main/java/login/filters/AuthFilter.java
@@ -8,6 +8,15 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * This class intercepts requests for pages (specified in web.xml) that need specific
+ * permissions to view. If the user doesn't have the required permission (eg. the user
+ * tries to view the admin panel and isn't an admin) the user is redirected to the
+ * login page.
+ *
+ * Note that this is an abstract class that provides the basis for the three different
+ * roles a user can have. Its subclasses are what are used in web.xml.
+ */
 public abstract class AuthFilter implements Filter {
     @Inject
     private AuthSessionManager authSessionManager;
@@ -26,8 +35,11 @@ public abstract class AuthFilter implements Filter {
         if (authSessionManager.isLoggedIn() && userRoleMatches(authSessionManager.getUser())) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
+            // TODO: change this if needed, it's just a placeholder
+            String loginPageUrl = "/login.xhtml";
             HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-            ((HttpServletResponse) servletResponse).sendRedirect(servletRequest.getServletContext().getContextPath() + "/nope.xhtml");
+            ((HttpServletResponse) servletResponse).sendRedirect(
+                    servletRequest.getServletContext().getContextPath() + loginPageUrl);
         }
     }
 
