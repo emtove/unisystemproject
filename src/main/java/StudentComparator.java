8c2661877616c2bc0f40d7a56442a433a5cc98dd
zpeedi <zpeedi@hotmail.com>
2017-04-25 15:50:52 +0200

    Här kommer teacherTakeAttendance Heidar! Part 2, more to come...

---
+++ src/main/java/StudentComparator.java
@@ -0,0 +1,13 @@
+import models.User;
+import java.util.Comparator;
+
+/**
+ * Created by Elev1 on 2017-04-24.
+ */
+public class StudentComparator implements Comparator<User> {
+        @Override
+        public int compare(User u1, User u2) {
+            return (int)(u1.getId()-u2.getId());
+        }
+    }
+
